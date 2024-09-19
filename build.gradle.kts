f344c3c28f2fb4c78aa3fee702cfe263a5232763
brandon <brandonchung123491@gmail.com>
2024-09-19 14:45:25 +0800

    added stage task to build.gradle.kts fot heroku deployment (trouble shooting is smooth so far)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,17 @@
 plugins {
     alias(libs.plugins.android.application) apply false
 }
+
+// Add the task to be recognized by Heroku
+tasks.register("stage") {
+    dependsOn("clean", "build")
+    doLast {
+        // Copy the JAR file to a location that Heroku recognizes
+        copy {
+            from(layout.buildDirectory.dir("libs"))
+            into(layout.buildDirectory.dir("staged"))
+            include("*.jar")
+            rename { "app.jar" }
+        }
+    }
+}
